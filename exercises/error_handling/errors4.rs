cddbe1b07be03d98bd9b03dd5840cdca33a8c061
Zan Baldwin <hello@zanbaldwin.com>
2023-02-26 18:07:53 +0100

    Sun Feb 26 18:07:53 CET 2023

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -1,7 +1,7 @@
 // errors4.rs
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::cmp::Ordering;
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
@@ -14,8 +14,11 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        match value.cmp(&0) {
+            Ordering::Less => Err(CreationError::Negative),
+            Ordering::Equal => Err(CreationError::Zero),
+            _ => Ok(PositiveNonzeroInteger(value as u64)),
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -23,6 +21,20 @@ struct Team {
     name: String,
     goals_scored: u8,
     goals_conceded: u8,
+}
+impl Team {
+    fn new(name: String) -> Self {
+        Self {
+            name,
+            goals_scored: 0,
+            goals_conceded: 0,
+        }
+    }
+
+    fn update(&mut self, scored: u8, conceded: u8) {
+        self.goals_scored += scored;
+        self.goals_conceded += conceded;
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -40,6 +52,16 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        let team_1_scores = scores
+            .entry(team_1_name.clone())
+            .or_insert(Team::new(team_1_name))
+            .update(team_1_score, team_2_score);
+
+        let team_2_scores = scores
+            .entry(team_2_name.clone())
+            .or_insert(Team::new(team_2_name))
+            .update(team_2_score, team_1_score);
     }
     scores
 }
